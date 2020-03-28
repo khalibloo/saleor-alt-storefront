0836feaebc996a9a8fd5ca309f21b0c5ccc35fca
Khalifa Lame <khalibloo@gmail.com>
2020-03-28 17:09:43 +0400

    Press any key to start

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Typography} from 'antd';
+import styles from "./index.less";
+
+export default () => {
+  return (
+    <div>
+      <Typography.Title level={1}>Saleor Alt Storefront</Typography.Title>
+    </div>
+  );
+};
