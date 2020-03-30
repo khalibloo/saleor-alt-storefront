54411b842a5c87609f121c62328376b9aad46e7a
Khalifa Lame <khalibloo@gmail.com>
2020-03-30 23:52:55 +0400

    feat(layout): styled global layout

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,16 +1,52 @@
 import React from "react";
-import { Typography, Layout, Menu, Row, Col, Input } from "antd";
-import { useIntl } from "umi";
-import { ShoppingCartOutlined, UserOutlined } from "@ant-design/icons";
+import {
+  Typography,
+  Layout,
+  Row,
+  Col,
+  Input,
+  //   Button,
+  Menu,
+  Dropdown,
+} from "antd";
+import { useIntl, Link } from "umi";
+import {
+  ShoppingCartOutlined,
+  UserOutlined,
+  GlobalOutlined,
+} from "@ant-design/icons";
+import clx from "classnames";
+
+import styles from "./index.less";
 
 const BasicLayout: React.FC = ({ children }) => {
   const intl = useIntl();
+  const langMenu = (
+    <Menu>
+      <Menu.Item>English</Menu.Item>
+      <Menu.Item>Français</Menu.Item>
+    </Menu>
+  );
+  const avatarMenu = (
+    <Menu>
+      <Menu.Item>
+        <Link to="/profile">My Profile</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to="/orders">My Orders</Link>
+      </Menu.Item>
+      <Menu.Divider />
+      <Menu.Item onClick={() => {}}>Logout</Menu.Item>
+    </Menu>
+  );
   return (
     <Layout className="full-height">
-      <Layout.Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+      <Layout.Header className={clx("full-width", styles.header)}>
         <Row justify="space-between" align="middle">
           <Col>
-            <Typography.Title level={4}>Alt Storefront</Typography.Title>
+            <Typography.Title level={3} className="no-margin">
+              Alt Storefront
+            </Typography.Title>
           </Col>
           <Col span={8}>
             <Input.Search
@@ -22,25 +58,49 @@ const BasicLayout: React.FC = ({ children }) => {
             />
           </Col>
           <Col>
-            <Menu mode="horizontal">
-              <Menu.Item key="1">
-                <ShoppingCartOutlined />
+            <Menu mode="horizontal" className="no-border" selectedKeys={[]}>
+              <Menu.Item key="1" className="no-padding">
+                <Dropdown overlay={langMenu}>
+                  <div className={styles.dropdownPadder}>
+                    <GlobalOutlined className={styles.navrightIcon} />
+                  </div>
+                </Dropdown>
               </Menu.Item>
               <Menu.Item key="2">
-                <UserOutlined />
+                <ShoppingCartOutlined className={styles.navrightIcon} />
+              </Menu.Item>
+              <Menu.Item key="3" className="no-padding">
+                <Dropdown overlay={avatarMenu} placement="bottomCenter">
+                  <div className={styles.dropdownPadder}>
+                    <UserOutlined className={styles.navrightIcon} />
+                  </div>
+                </Dropdown>
               </Menu.Item>
             </Menu>
+            {/* <Row>
+              <Col>
+                <Button size="large" className="icon-btn">
+                  <ShoppingCartOutlined className={styles.navrightIcon} />
+                </Button>
+              </Col>
+              <Col>
+                <Button size="large" className={"icon-btn"}>
+                  <UserOutlined className={styles.navrightIcon} />
+                </Button>
+              </Col>
+            </Row> */}
           </Col>
         </Row>
       </Layout.Header>
-      <Layout.Content
-        className="site-layout"
-        style={{ padding: "0 50px", marginTop: 64 }}
-      >
-        {children}
-      </Layout.Content>
-      <Layout.Footer style={{ textAlign: "center" }}>
-        {intl.formatMessage({ id: "footer.createdby" })}
+      <Layout.Content className={styles.content}>{children}</Layout.Content>
+      <Layout.Footer>
+        <Row justify="center">
+          <Col>
+            <Typography.Text className="center-text">
+              {intl.formatMessage({ id: "footer.createdby" })}
+            </Typography.Text>
+          </Col>
+        </Row>
       </Layout.Footer>
     </Layout>
   );
