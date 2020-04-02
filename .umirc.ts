f2a00292b951a22a4859a69f6e87823da0ad7bc7
Khalifa Lame <khalibloo@gmail.com>
2020-04-02 12:08:09 +0400

    perf: extract critical css

--- .umirc.ts
+++ .umirc.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "umi";
+import path from "path";
+import HtmlCriticalWebpackPlugin from "html-critical-webpack-plugin";
 
 export default defineConfig({
   title: "Alt Storefront",
@@ -6,15 +8,43 @@ export default defineConfig({
     default: "en-US",
     antd: true,
     title: true,
-    baseNavigator: true,
+    baseNavigator: false,
     baseSeparator: "-",
   },
+  links: [
+    {
+      rel: "preconnect",
+      href: "https://via.placeholder.com",
+    },
+  ],
   define: {
     APP_ENV: "default",
+    ENDPOINT: "",
     SITE_NAME: "Alt Storefront",
   },
   theme: {
     "layout-header-background": "#fff",
     "layout-body-background": "#fff",
+  },
+  chainWebpack: (memo, {}) => {
+    memo
+      .plugin("critical")
+      .use(HtmlCriticalWebpackPlugin)
+      .init(
+        (Plugin, args) =>
+          new HtmlCriticalWebpackPlugin({
+            base: path.resolve(__dirname, "dist"),
+            src: "index.html",
+            dest: "index.html",
+            inline: true,
+            minify: true,
+            extract: true,
+            width: 375,
+            height: 565,
+            penthouse: {
+              blockJSRequests: false,
+            },
+          }),
+      );
   },
 });
