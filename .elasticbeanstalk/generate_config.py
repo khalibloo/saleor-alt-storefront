8962b06563d0e032e4552c8afbb87a443a9503dc
Khalifa Lame <khalibloo@gmail.com>
2020-06-10 15:05:21 +0400

    chore: cleanup eb config experiments

--- .elasticbeanstalk/generate_config.py
+++ .elasticbeanstalk/generate_config.py
@@ -9,14 +9,13 @@ content = """branch-defaults:
 global:
   application_name: $AWS_EB_APP_NAME
   default_ec2_keyname: $AWS_EB_EC2_KEY_NAME
-  default_platform: 64bit Amazon Linux 2017.09 v2.6.2 running Ruby 2.4 (Passenger Standalone)
+  default_platform: Docker running on 64bit Amazon Linux 2/3.0.2
   default_region: $AWS_REGION
   sc: git
 """
 env_keys: List[str] = [word for word in content.split() if word.startswith("$")]
 for key in env_keys:
   value = os.environ.get(key[1:])
-  print(key + ": " + value)
   content = content.replace(key, value)
 
 with open(os.path.join(os.path.dirname(__file__), "config.yml"), "w+") as file:
