1db0fc8a5d28844bfcdb1e8fdd8f2cfe1a3595a0
Khalifa Lame <khalibloo@gmail.com>
2020-09-17 13:47:11 +0400

    feat: show message when offline

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,7 @@
 // import * as Sentry from "@sentry/browser";
 import { configResponsive } from "@umijs/hooks";
+import { message } from "antd";
+import { getIntl } from "umi";
 
 // import config from "./config";
 
@@ -12,6 +14,30 @@ configResponsive({
   lg: 992,
   xl: 1200,
   xxl: 1600,
+});
+
+const intl = getIntl();
+window.addEventListener("load", function() {
+  const key = "offline";
+  const offlineMsg = () =>
+    message.error({
+      content: intl.formatMessage({ id: "misc.offline.msg" }),
+      duration: 20,
+      key,
+      onClose: offlineMsg,
+    });
+  window.addEventListener("offline", offlineMsg);
+
+  window.addEventListener("online", () => {
+    message.error({
+      content: intl.formatMessage({ id: "misc.offline.msg" }),
+      duration: 0.01,
+      key,
+    });
+    message.success({
+      content: intl.formatMessage({ id: "misc.online.msg" }),
+    });
+  });
 });
 
 // configure sentry
